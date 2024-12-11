fb87ad5fc7c4707113d32183e9b07e3f0e5878a2
1961769 <1961769@01-18108.pusd.dom>
2024-12-11 13:51:40 -0800

    commit?

--- Advent/src/days/Day3.java
+++ Advent/src/days/Day3.java
@@ -5,54 +5,39 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day3 {
-//	public static boolean corruptOrNot(String[] list) {
-//		int errorCount = 0;
-//		int tolerance = 1;
-//		for(int i = 1; i < list.length; i++) {
-//			//check inverse
-//			int el1= Integer.valueOf(list[i]);
-//			int el0= Integer.valueOf(list[i-1]);
-//			
-//			if(el1 <= el0) {
-//				return false;
-//			}
-//			if(el1 - el0 > 3 && (errorCount <= tolerance)) {
-//				errorCount += 1;
-//				return true;
-//			}
-//			else if(el1 - el0 > 3 && (errorCount > tolerance)) {
-//				errorCount += 1;
-//				return false;
-//			}
-//			
-//		}
-//		//no inverse output
-//		return true;
-//	}
-	
+
 	public static void main(String[] args) {
-		
-		
-		File list = new File("day2input.txt");
-		
-		try {
-			//
-			Scanner scan = new Scanner(list);
-			int count = 0;
-			while(scan.hasNext()) {
-				//grab one line
-				String line = scan.nextLine();
-				
-				//split the line into string array
-				String[] elements = line.split(" ");
-				
-			}
-			System.out.println("Total safe...is " + count);
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		 String corruptedMemory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+	        
+	        // Call the function to calculate the sum of the multiplications
+	        int result = sumMultiplications(corruptedMemory);
+	        
+	        // Print the result
+	        System.out.println(result);
 	}
+	
+	   public static int sumMultiplications(String corruptedMemory) {
+	        // Regular expression to match valid mul(X,Y) instructions
+	        String regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(corruptedMemory);
+
+	        int totalSum = 0;
+
+	        // Find all valid mul(X,Y) instructions
+	        while (matcher.find()) {
+	            // Extract the numbers X and Y
+	            int x = Integer.parseInt(matcher.group(1));
+	            int y = Integer.parseInt(matcher.group(2));
+
+	            // Multiply them and add the result to the total sum
+	            totalSum += x * y;
+	        }
+
+	        return totalSum;
+	    }
 }
